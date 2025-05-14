b026661b3f993624a016c90764a3aca65b92d864
SuSom02 <ljsaxwoex9349@gmail.com>
2025-05-15 06:08:22 +0900

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/wiffle/',  // GitHub Pages에서 호스팅되는 경로
 })
